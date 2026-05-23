e0b53a16f57a58928609e19df6214319844712d9
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-05-23 15:23:34 +0530

    Delete Print Message directory

--- Print Message/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-//Write a program to print Hello World?
-public class HelloWorld{
-    public static void main(String args[]){
-        //To print Hello World 
-        System.out.println("Hello World!");
-    }
-}
--- Print Message/PrintName.java
+++
@@ -1,7 +0,0 @@
-//Write a program to print Name
-public class PrintName {
-    public static void main(String args[]){
-        //To print Name
-        System.out.println("Ashu Sharma");
-    }
-}
